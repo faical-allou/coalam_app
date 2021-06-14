044a9e506456691aaaaa209585331855f79e241c
Faical Allou <faical.allou@gmail.com>
2021-06-14 20:56:44 +0200

    improve UX for events

--- lib/screens/RecipeDetailsScreen.dart
+++ lib/screens/RecipeDetailsScreen.dart
@@ -54,6 +54,11 @@ class RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                                     ],
                                     options: CarouselOptions(
                                       height: 200,
+                                      viewportFraction: 0.2,
+                                      initialPage: 0,
+                                      enableInfiniteScroll: false,
+                                      reverse: false,
+                                      enlargeCenterPage: true,
                                     )),
                               );
                             } else {
@@ -109,12 +114,15 @@ class RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                                   CoalamCard(Row(children: [
                                     Expanded(
                                         child: Text(DateFormat('d-MMM-y HH:mm')
-                                            .format(DateTime.parse(snapshot2
-                                                .data![i]['start']['dateTime']).toLocal())
+                                            .format(DateTime.parse(
+                                                    snapshot2.data![i]['start']
+                                                        ['dateTime'])
+                                                .toLocal())
                                             .toString())),
                                     Expanded(
-                                        child: Text(
-                                            snapshot2.data![i]['description'] ?? "")),
+                                        child: Text(snapshot2.data![i]
+                                                ['description'] ??
+                                            "")),
                                     TextButton(
                                       child: Text('check it'),
                                       onPressed: () => {
@@ -122,79 +130,104 @@ class RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                                             snapshot2.data![i]['hangoutLink'])
                                       },
                                     ),
-                                    TextButton(
-                                      child: Text('delete it'),
-                                      onPressed: () => {
+                                    _getDeleteLink(snapshot2.data![i]['id'], setState)
 
-                                        deleteEvent(
-                                            snapshot2.data![i]['id'], setState ),
-                                      },
-                                    ),
                                   ])),
                               ]);
                             } else {
                               return Text('fetching events');
                             }
                           }),
+                      Consumer<GlobalState>(builder: (context, status, child) {
+                        var status = context.read<GlobalState>();
+                        if (!status.isLoggedIn || snapshot.data!.chefId != status.chefId) {
+                          return Container();
+                        } else {
+                          return CoalamCard(
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    width: 100,
+                                    child: TextButton(
+                                      child: Text(
+                                        DateFormat("d-MMM-y @ HH:mm")
+                                            .format(selectedDateTime)
+                                            .toString(),
+                                      ),
+                                      onPressed: () {
+                                        DatePicker.showDateTimePicker(context,
+                                            showTitleActions: true,
+                                            minTime: DateTime.now(),
+                                            maxTime: DateTime(2019, 6, 7),
+                                            onChanged: (date) {
+                                          print('change $date');
+                                        }, onConfirm: (date) {
+                                          setState(() {
+                                            selectedDateTime = date;
+                                          });
+                                        },
+                                            currentTime: selectedDateTime,
+                                            locale: LocaleType.en);
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.30,
+                                      child: CoalamTextInputField(
+                                          "",
+                                          descriptionInput,
+                                          Text('Title').data,
+                                          60,
+                                          1,
+                                          30)),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.green,
+                                    ),
+                                    child: Text('+ add event'),
+                                    onPressed: () {
+                                      var start =
+                                          DateFormat("yyyy-MM-ddTHH:mm:ss")
+                                              .format(selectedDateTime.toUtc());
+                                      var end =
+                                          DateFormat("yyyy-MM-ddTHH:mm:ss")
+                                              .format(selectedDateTime
+                                                  .toUtc()
+                                                  .add(new Duration(hours: 1)));
 
-              CoalamCard(Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(width: 100,
-                              child: TextButton(
-                              child: Text(
-                                DateFormat("d-MMM-y @ HH:mm")
-                                    .format(selectedDateTime)
-                                    .toString(),
-                              ),
-                              onPressed: () {
-                                DatePicker.showDateTimePicker(context,
-                                    showTitleActions: true,
-                                    minTime: DateTime.now(),
-                                    maxTime: DateTime(2019, 6, 7),
-                                    onChanged: (date) {
-                                  print('change $date');
-                                }, onConfirm: (date) {
-                                  setState(() {
-                                    selectedDateTime = date;
-                                  });
-                                },
-                                    currentTime: selectedDateTime,
-                                    locale: LocaleType.en);
-                              },
-                            ),),
-                            Container(width: MediaQuery.of(context).size.width * 0.30,
-                                child: CoalamTextInputField(
-                                "",  descriptionInput,
-                                Text('Title').data, 60, 1, 30)),
-
-                      ElevatedButton(
-                        child: Text('+ add event'),
-                        onPressed: () {
-                          var start = DateFormat("yyyy-MM-ddTHH:mm:ss")
-                              .format(selectedDateTime.toUtc());
-                          var end = DateFormat("yyyy-MM-ddTHH:mm:ss")
-                              .format(selectedDateTime.toUtc()
-                              .add(new Duration(hours: 1)));
-
-                          isValidEvent(snapshot.data!.chefId,
-                              int.parse(widget.id!),
-                              start,
-                              end,
-                              descriptionInput.text)
-                          ? () {createEvent(
-                              snapshot.data!.chefId,
-                              int.parse(widget.id!),
-                              start,
-                              end,
-                              descriptionInput.text,
-                              setState
+                                      isValidEvent(
+                                              snapshot.data!.chefId,
+                                              int.parse(widget.id!),
+                                              start,
+                                              end,
+                                              descriptionInput.text)
+                                          ? () {
+                                              createEvent(
+                                                  snapshot.data!.chefId,
+                                                  int.parse(widget.id!),
+                                                  start,
+                                                  end,
+                                                  descriptionInput.text,
+                                                  setState);
+                                              showAlertDialogValidation(context,
+                                                  Text("Nice !!").data ?? "",
+                                                  Text("Your event will be live in a moment").data ?? "",
+                                                  Text("Go Back").data ?? "");
+                                            }()
+                                          : showAlertDialogValidation(context,
+                                        Text("Oops something is missing").data ?? "",
+                                        Text("Make sure you have a description").data ?? "",
+                                        Text("Go Back").data ?? "");
+                                    },
+                                  ),
+                                ]),
                           );
-                          }()
-                          : showAlertDialogValidation(context);
-                        },
-                      ),]),),
+                        }
+                      }),
                       TextButton(
                         child: Text('back!'),
                         onPressed: () {
@@ -204,6 +237,8 @@ class RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                     ],
                   ),
                 ),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.endTop,
                 floatingActionButton: _getFAB(snapshot.data, mainImage));
           } else {
             return CircularProgressIndicator();
@@ -229,10 +264,11 @@ class ImageCarousel extends StatelessWidget {
 Widget _getFAB(Recipe? inputRecipe, Image inputImage) {
   return Consumer<GlobalState>(builder: (context, status, child) {
     var status = context.read<GlobalState>();
-    if (!status.isLoggedIn) {
+    if (!status.isLoggedIn || inputRecipe!.chefId != status.chefId) {
       return Container();
     } else {
       return FloatingActionButton(
+          backgroundColor: Colors.green,
           child: Icon(Icons.edit),
           onPressed: () {
             Navigator.push(
@@ -244,30 +280,87 @@ Widget _getFAB(Recipe? inputRecipe, Image inputImage) {
     }
   });
 }
+Widget _getDeleteLink(id, function) {
+  return Consumer<GlobalState>(builder: (context, status, child) {
+    var status = context.read<GlobalState>();
+    if (!status.isLoggedIn) {
+      return Container();
+    } else { return
+      TextButton.icon(
+        style: TextButton.styleFrom(
+          primary: Colors.red,
+        ),
+        icon:Icon(Icons.delete),
+        label: Text(''),
+        onPressed: () =>
+        { showAlertDialogDelete(context, id, function),
+        },
+      );
+  }
+  });}
 
-bool isValidEvent(int? int1, int? int2, String? text1, String? text2, String? text3){
+bool isValidEvent(
+    int? int1, int? int2, String? text1, String? text2, String? text3) {
   print(text3);
   print(text3?.length);
-  bool test = (int1 != null) & (int2 != null) & (text1 != null) & (text2 != null)
-                & (text3 != null) & (text3!.length >= 3);
+  bool test = (int1 != null) &
+      (int2 != null) &
+      (text1 != null) &
+      (text2 != null) &
+      (text3 != null) &
+      (text3!.length >= 3);
   return test;
 }
 
-showAlertDialogValidation(BuildContext context) {
-
+showAlertDialogValidation(BuildContext context, String text1, String text2, String text3) {
   Widget cancelButton = TextButton(
-    child: Text("Go back"),
+    child: Text(text3),
     onPressed: () {
       Navigator.pop(context);
     },
   );
 
   AlertDialog alert = AlertDialog(
-    title: Text("Oops something is missing"),
-    content: Text("Make sure you have a description"),
+    title: Text(text1),
+    content: Text(text2),
     actions: [
       cancelButton,
     ],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+showAlertDialogDelete(BuildContext context, eventId, function) {
+
+  Widget continueButton = TextButton(
+    child: Text("yes, delete"),
+    onPressed:  () {
+      deleteEvent(eventId, function);
+      Navigator.pop(context);
+    },
+  );
+
+  Widget cancelButton = TextButton(
+    child: Text("cancel"),
+    onPressed:  () {
+      Navigator.pop(context);
+    },
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: Text("Are you sure you want to delete?"),
+    content: Text("There's no turning back!"),
+    actions: [
+      cancelButton,
+      continueButton,
+    ],
+
   );
 
   showDialog(
